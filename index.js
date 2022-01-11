5b1351316550f0b886b87b69bc6de317ec93f8f5
abesushihonten <97285350+abesushihonten@users.noreply.github.com>
2022-01-11 20:49:20 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ $(function(){
 	$("h1").fadeIn(1500,function(){
 		$("#btns").fadeIn(1500);
 	});
-	$(".fa-bars").on("click",function(){
-		$(".side-container").toggle("slideDown");
+	$(".fa-bars").click(function(){
+		if($("#menu").hasClass("hide")){
+			$("#menu").slideDown().removeClass("hide");
+		}else{
+			$("#menu").slideUp().addClass("hide");
+		}
 	});
+
 });
--- shop.js
+++ shop.js
@@ -1,9 +1,18 @@
 $(function(){
 	$("#title").fadeIn(1500);
 	$(".fa-bars").on("click",function(){
-		$(".side-container").toggle("slideDown");
+		$(".side-container").toggle();
 	});
-
+	$(".fa-shopping-cart").on("click",function(){
+    $(".cart").toggle();
+  });
+	$(".fa-bars").click(function(){
+    if($("#menu").hasClass("hide")){
+      $("#menu").slideDown().removeClass("hide");
+    }else{
+      $("#menu").slideUp().addClass("hide");
+    }
+  });
 	$(function(){
   $('a[href^="#"]').click(function(){
     var speed = 500;
